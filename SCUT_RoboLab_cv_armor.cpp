f64e406a6079890927b2cbe2bb2598ac8986d25a
Xingzhao.H <huang.xing.zhao@qq.com>
2019-10-26 10:49:33 +0800

    暂停与前进后退“

--- SCUT_RoboLab_cv_armor.cpp
+++ SCUT_RoboLab_cv_armor.cpp
@@ -1,7 +1,7 @@
 ﻿#include "Functions.h"
 #include <iostream>
 #include <opencv2/opencv.hpp>
-#include <boost/progress.hpp>
+#include <boost/timer.hpp>
 #include <vector>
 #include <cmath>
 #define is_Red(i) ((i>156)||(i<10))
@@ -13,19 +13,14 @@
 	#define TARGET(i) is_Blue(i)
 #endif
 
-constexpr auto _VEDIO = "../resource/armor.mp4";
+constexpr auto _VEDIO = "../resource/armor_2.mkv";
 int main()
 {
-	int n = 0;
+	int flag_play = 100;
 	cv::VideoCapture cap(_VEDIO);
-	//cv::namedWindow("R");
-	//int shold_median = 5;
-	//int shold = 16;
-	//cv::createTrackbar("shold: ", "R", &shold_median, 20);
+
 	while (1)
 	{
-		++n;
-		std::cout << "\n------------------\n";
 		boost::timer g_time;
 		cv::Mat frame;
 		cap >> frame;
@@ -34,37 +29,35 @@ int main()
 		std::vector<cv::Mat> channels_unB;
 		cv::Mat Hue_sholded, Hue_unB_sholded, Value_unB_sholded;
 		cv::Mat R;
-
 		std::vector<std::vector<cv::Point>> contours, contours_Tar;
 		std::vector<cv::Vec4i> hierarchy;
 		std::vector<cv::RotatedRect> rRect,rRect_Tar;
 		std::vector<armor> rRect_match;
-		#ifdef Debug
-		std::cout << "Create: " << g_time.elapsed() << std::endl;
-		#endif
+		switch (cv::waitKey(flag_play))
+		{
+
+			case 32:
+			flag_play=flag_play==0?100:0;
+			break;
+
+			case 100:
+			cap.set(cv::CAP_PROP_POS_FRAMES,cap.get(cv::CAP_PROP_POS_FRAMES));
+			break;
+
+			case 97:
+			cap.set(cv::CAP_PROP_POS_FRAMES,cap.get(cv::CAP_PROP_POS_FRAMES)-2);
+			break;
+			default:
+			break;
+		}
 		if (frame.empty())
 		{
-			cap.release();
-			cap.open(_VEDIO);
+			cap.set(cv::CAP_PROP_POS_FRAMES,0);
 			continue;
-			n=0;
 		}
 
-		{
-			#ifdef Debug
-			boost::progress_timer t;
-			#endif
 			colorReduce(frame, reduce, 16);
 			cvtColor(reduce, HSV, cv::COLOR_BGR2HSV);
-			#ifdef Debug
-			std::cout << "colorReduce: ";
-			#endif
-		}
-
-		{
-			#ifdef Debug
-			boost::progress_timer t;
-			#endif
 			split(HSV, channels_unB);
 			myShold(channels_unB[0], Hue_unB_sholded, [](int i)->uchar {return TARGET(i) ? 255 : 0; });
 			cv::medianBlur(Hue_unB_sholded, Hue_sholded, 3);
@@ -72,15 +65,9 @@ int main()
 			R = Hue_sholded & Value_unB_sholded ;
 			cv::morphologyEx(R, R, cv::MORPH_CLOSE, cv::getStructuringElement(cv::MORPH_CROSS, cv::Size(3, 3), cv::Point(-1, -1)));
 			cv::morphologyEx(R, R, cv::MORPH_CLOSE, cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(3, 3), cv::Point(-1, -1)));
-			#ifdef Debug
-			std::cout << "shold and morphology: ";
-			#endif
-		}
-				cv::Mat con(R.rows, R.cols, CV_8UC3, cv::Scalar(0, 0, 0));
-		{
-			#ifdef Debug
-			boost::progress_timer t;
-			#endif
+			
+			cv::Mat con(R.rows, R.cols, CV_8UC3, cv::Scalar(0, 0, 0));
+
 			cv::findContours(R, contours, hierarchy, cv::RETR_CCOMP, cv::CHAIN_APPROX_TC89_L1);
 			for (std::vector<cv::Vec4i>::iterator i = hierarchy.begin(); i != hierarchy.end(); ++i)
 			{
@@ -90,16 +77,10 @@ int main()
 				}
 			}
 			cv::drawContours(con, contours_Tar, -1, cv::Scalar(255, 255, 255));
-			#ifdef Debug
-			std::cout << "findContours: ";
-			#endif
-		}
+
 			//cv::morphologyEx(con, con, cv::MORPH_CLOSE, cv::getStructuringElement(cv::MORPH_RECT, cv::Size(3, 3)));
 
-		{
-			#ifdef Debug
-			boost::progress_timer t;
-			#endif			
+
 			for (auto i : contours_Tar)
 			{
 				rRect.push_back(cv::minAreaRect(i));
@@ -127,14 +108,8 @@ int main()
 					line(con, vertices[j], vertices[(j + 1) % 4], cv::Scalar(179, 245, 222), 1);
 				}
 			}
-			#ifdef Debug
-			std::cout<<"find Rect: ";
-			#endif
-		}
-		{
-			#ifdef Debug
-			boost::progress_timer t;
-			#endif
+
+
 			for (auto i = rRect_Tar.begin(); i != rRect_Tar.end(); ++i)
 			{
 				if (i->angle == 0)
@@ -167,26 +142,13 @@ int main()
 					line(frame, i.points[j], i.points[(j + 1) % 4], cv::Scalar(0, 255, 0), 1);
 				}
 			}
-			#ifdef Debug
-			std::cout<<"match Rects: ";
-			#endif
-		}
 
-		{
-			#ifdef Debug
-			boost::progress_timer t;
-			#endif
 			cv::drawContours(con, contours_Tar, -1, cv::Scalar(255, 255, 255),1);
 			imshow("con", con);
 			imshow("frame", frame);
+			imshow("HSV",HSV);
 			//imshow("R", R);
-			#ifdef Debug
-			std::cout<<"output: ";
-			#endif
-		}
-		std::cout << "global time: " << g_time.elapsed() << std::endl;;
-		std::cout << n ;
-		cv::waitKey(1);
+		 std::cout << cap.get(cv::CAP_PROP_POS_FRAMES)<<":"<<g_time.elapsed()<<std::endl;
 	}
 }
 
